b2fedb3f8cc8aae40dce979ad8ccc71a8009fd63
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-22 03:06:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock)  {
     let start = parseInt( startBlock );
     let end = parseInt( endBlock );
-    let travelRange = 
+    let travelRange = Math.abs( end - start );
+    let 
     if (start - end <= blockRange)
       return `within range by ${travelRange}`;
       
